aae6fea097f9de61bd4cdf790abf499dc6be5aee
Hicham Ferhani <hicham.ferhani@etu.umontpellier.fr>
2024-09-11 09:51:12 +0200

    feat : ajout de tests unitaires pour la classe Document

---
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -0,0 +1,33 @@
+package fr.iut.editeur.document;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * AJout de tests unitaires testant les différentes méthodes dans la classe Document
+ */
+
+public class DocumentTest {
+    Document document = new Document();
+    @Test
+    public void testAjouter() {
+        document.ajouter("bonjour");
+        assertTrue(document.getTexte().equals("bonjour"));
+    }
+
+    @Test
+    public void testMajuscule() {
+        document.ajouter("bonjour");
+        document.majuscules(0, document.getTexte().length() - 1);
+        assertTrue(document.getTexte().equals("BONJOUR"));
+    }
+
+    @Test
+    public void testEffacer(){
+        document.ajouter("bonjour");
+        document.effacer(0,0);
+        assertTrue(document.getTexte().equals("onjour"));
+    }
+
+}
